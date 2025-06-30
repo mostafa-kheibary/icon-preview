0aa537f36db566377ec716ccb112ef3cc54f454a
Mostafa Kheibary <mostafa.kheibary@gmail.com>
2025-06-30 16:01:40 +0330

    version 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
+#!/usr/bin/env node
+
 import fs from "fs/promises";
+import path from "path";
 import recursive from "recursive-readdir";
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
-const iconPaths = {};
+let iconPath = {};
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-console.log(process.argv);
+const processIconFiles = async () => {
+  let files = await recursive(process.argv[2]);
+  files = files.filter((file) => file.endsWith(".svg"));
 
-// recursive(process.argv)
+  for (const file of files) {
+    const content = await fs.readFile(path.resolve("./", file), {
+      encoding: "utf8",
+    });
+    const name = file.split("/").at(-1);
+    iconPath[name] = content;
+  }
+};
+
+processIconFiles();
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./views"));
+
+app.get("/", (req, res) => {
+  res.render("index", { icons: iconPath });
+});
+
+const PORT = 4000;
+app.listen(PORT);
+
+console.log(`preview of ${process.argv[2]} is available at localhost:${PORT}`);
